7c365d39bc1c09ea3c7cf3937c344440582be140
klaygore <seetheclay@live.com>
2019-09-01 01:46:54 -0500

    changed layout to add tap/hold for right shift and enter. also moved function 2 button to make using the reset command easier.

--- keyboards/boardwalk/keymaps/clay/keymap.c
+++ keyboards/boardwalk/keymaps/clay/keymap.c
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      |   6    | 7      | 8      | 9      | 0      |  -     | =      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    |  [     | Q      | W      | E      | R      |   T    | Y      | U      | I      | O      |  P     | ]      | \      |
+ * | TAB    |  [     | Q      | W      | E      | R      |   T    | Y      | U      | I      | O      |  P     | ]      | GUI \  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * |SHFTCAPS|  DEL   | A      | S      | D      | F      |   G    | H      | J      | K      | L      |  ;:    |  '"    | ENTER  |
+ * |SFTCAPS |  DEL   | A      | S      | D      | F      |   G    | H      | J      | K      | L      |  ;:    |  '"    |SFTENT  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |KC_PSCR | END    |
+ * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |PRNTSCN | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
  * | LCTRL  | PG DN  | LALT   |FN      |  LGUI  |      SPACE      |      SPACE      |  LEFT  | UP     | RIGHT  | DOWN   | RCTRL  |
  * '-----------------------------------------------------------------------------------------------------------------------------'
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_1U] = LAYOUT_ortho_5x14(
   KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,   KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
-  RSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  \
+  LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
   KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_PSCR, KC_END, \
   KC_LCTL,  		KC_PGDN, KC_LALT, TT(1),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  KC_LEFT, KC_UP,  KC_RGHT, KC_DOWN, KC_RCTL  \
  ),
