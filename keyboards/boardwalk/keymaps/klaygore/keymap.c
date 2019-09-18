4025d24f19844008d5576ce075255bfa301e4ae5
klaygore <seetheclay@live.com>
2019-09-18 14:12:21 -0500

    corrected comments, changed print screen to find, added lighting controls

--- keyboards/boardwalk/keymaps/klaygore/keymap.c
+++ keyboards/boardwalk/keymaps/klaygore/keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * |SFTCAPS |  DEL   | A      | S      | D      | F      |   G    | H      | J      | K      | L      |  ;:    |  '"    |SFTENT  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |PRNTSCN | END    |
+ * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |  UP    | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | PG DN  | LALT   |FN      |  LGUI  |      SPACE      |      SPACE      |  LEFT  | UP     | RIGHT  | DOWN   | RCTRL  |
+ * | LCTRL  | PG DN  | LALT   |  FN    |  LGUI  |      SPACE      |      SPACE      |  FN2   |  FIND  | RIGHT  | DOWN   |CTRLRGHT|
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
   LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
   KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_END, \
-  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_PSCR, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
+  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_FIND, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
 /* custom linux - layer 1
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * |SFTCAPS |        | A      | S      | D      | F      |   G    | H      | J      | K      | L      |  ;:    |  '"    |SFTENT  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |PRNTSCN | END    |
+ * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |  UP    | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | PG DN  | LALT   |FN      |  LGUI  |      SPACE      |      SPACE      |  LEFT  | UP     | RIGHT  | DOWN   | RCTRL  |
+ * | LCTRL  | PG DN  | LALT   |  FN    |  LGUI  |      SPACE      |      SPACE      |  FN2   |  FIND  | RIGHT  | DOWN   |CTRLRGHT|
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
   LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
   KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_END, \
-  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_PSCR, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
+  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_FIND, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
 
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
  ),
 
 /* FUNCTION3 - layer 4
@@ -152,8 +152,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   DF(0),   DF(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MODE_REVERSE, RGB_VAD, RGB_MODE_FORWARD \
  ),
 
  
