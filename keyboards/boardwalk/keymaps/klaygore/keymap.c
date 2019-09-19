a35cd837a8f710e33e4c4f9a0286702a179c5893
klaygore <seetheclay@live.com>
2019-09-18 22:10:57 -0500

    swapped the kc_find command for lctrl(kc_f) on main and linux layers

--- keyboards/boardwalk/keymaps/klaygore/keymap.c
+++ keyboards/boardwalk/keymaps/klaygore/keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_main] = LAYOUT_ortho_5x14(
-  KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-  KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
-  LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
-  KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_END, \
-  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_FIND, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
+  KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,       KC_MINS, KC_EQL,  KC_BSPC, \
+  KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,       KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
+  LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,       KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
+  KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,     KC_SLSH, KC_UP,   KC_END, \
+  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   LCTL(KC_F), KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
 /* custom linux - layer 1
@@ -79,11 +79,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_linux] = LAYOUT_ortho_5x14(
-  KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-  KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
-  LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
-  KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_END, \
-  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   KC_FIND, KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
+  KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,       KC_MINS, KC_EQL,  KC_BSPC, \
+  KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,       KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
+  LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,       KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
+  KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,     KC_SLSH, KC_UP,   KC_END, \
+  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   LCTL(KC_F), KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
 
