fd07f9674ff5d85b2db0177d8f3ef41ae7e150c4
klaygore <seetheclay@live.com>
2019-09-28 08:39:00 -0500

    renamed linux layer to gaming, removed left alt and left gui

--- keyboards/boardwalk/keymaps/klaygore/keymap.c
+++ keyboards/boardwalk/keymaps/klaygore/keymap.c
@@ -34,7 +34,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // Layer shorthand
 enum layer {
   _main,
-  _linux,
+  _gaming,
   _FN,
   _FN2,
   _FN3,
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_BSPC, KC_BSPC,  KC_SPC,   KC_SPC,  TT(3),   LCTL(KC_F), KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
-/* custom linux - layer 1
+/* custom gaming - layer 1
  * .-----------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |   6    | 7      | 8      | 9      | 0      |  -     | =      | GRAVE  |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      |   6    | 7      | 8      | 9      | 0      |  -     | =      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    |  [     | Q      | W      | E      | R      |   T    | Y      | U      | I      | O      |  P     | ]      | GUI \  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
@@ -74,16 +74,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | HOME   | PG UP  | Z      | X      | C      | V      |   B    | N      | M      | ,      | .      |  /     |  UP    | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | PG DN  | LALT   |  FN    |  LGUI  |      BACKSP     |      SPACE      |  FN2   |  FIND  | RIGHT  | DOWN   |CTRLRGHT|
+ * | LCTRL  | PG DN  |        |  FN    |        |      BACKSP     |      SPACE      |  FN2   |  FIND  | RIGHT  | DOWN   |CTRLRGHT|
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
- [_linux] = LAYOUT_ortho_5x14(
+ [_gaming] = LAYOUT_ortho_5x14(
   KC_ESC,   		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,       KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,   		KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,       KC_P,    KC_RBRC, RGUI_T(KC_BSLS), \
   LSFT_T(KC_CAPS),  KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,       KC_SCLN, KC_QUOT, RSFT_T(KC_ENT),  \
   KC_HOME,  		KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,     KC_SLSH, KC_UP,   KC_END, \
-  KC_LCTL,  		KC_PGDN, KC_LALT, TT(2),   KC_LGUI, KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   LCTL(KC_F), KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
+  KC_LCTL,  		KC_PGDN, KC_NO,   TT(2),   KC_NO,   KC_SPC,  KC_SPC,   KC_SPC,   KC_SPC,  TT(3),   LCTL(KC_F), KC_LEFT, KC_DOWN, RCTL_T(KC_RGHT)  \
  ),
 
 
