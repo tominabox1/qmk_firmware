a3d1e024eb7cdddb54863fe222e30d92a6dc4e7a
klaygore <seetheclay@live.com>
2019-10-13 09:06:05 -0500

    added lalt back to broken key for testing

--- keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
+++ keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,    KC_W,     KC_E,     KC_R,            KC_T,     KC_Y,    KC_U,    KC_I,           KC_O,     KC_P,     KC_BSLS,      KC_P7,       KC_P8,          KC_P9,       RALT_T(KC_PAST),
     LSFT_T(KC_CAPS), KC_A,    KC_S,     KC_D,     KC_F,            KC_G,     KC_H,    KC_J,    KC_K,           KC_L,     KC_SCLN,  KC_QUOT,      KC_P4,       RGUI_T(KC_P5),  KC_P6,       TD(DSH_UND),
     KC_LCTL,         KC_Z,    KC_X,     KC_C,     KC_V,            KC_B,     KC_N,    KC_M,    KC_COMM,        KC_DOT,   KC_SLSH,  KC_UP,        KC_P1,       KC_P2,          KC_P3,       TD(PLS_EQL),
-    KC_LALT,  	     KC_NO,   TT(2),    KC_LGUI,  LALT_T(KC_BSPC), KC_BSPC,  KC_SPC,  KC_SPC,  RSFT_T(KC_ENT), TT(3),    KC_LEFT,  KC_DOWN,      KC_RIGHT,    KC_0,           KC_PDOT,     RCTL_T(KC_ENT)
+    KC_LALT,  	     KC_LALT,   TT(2),    KC_LGUI,  LALT_T(KC_BSPC), KC_BSPC,  KC_SPC,  KC_SPC,  RSFT_T(KC_ENT), TT(3),    KC_LEFT,  KC_DOWN,      KC_RIGHT,    KC_0,           KC_PDOT,     RCTL_T(KC_ENT)
   ),
 
 /* [1] GAME
