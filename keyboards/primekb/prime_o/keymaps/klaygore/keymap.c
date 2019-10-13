4adf948e340abd8de9ae14925dac83069950b982
klaygore <seetheclay@live.com>
2019-10-13 14:10:51 -0500

    changed game layer to use function keys instead of numbers

--- keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
+++ keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
@@ -99,11 +99,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [1] = LAYOUT(
-    KC_ESC,   	     KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,           KC_9,       KC_0,     KC_BSPC, KC_NLCK,    KC_MPLY,   TD(NXT_PRV), KC_PSLS,
+    KC_ESC,   	     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,          KC_F9,      KC_F10,   KC_BSPC, KC_NLCK,    KC_MPLY,   TD(NXT_PRV), KC_PSLS,
     KC_TAB,    	     KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,           KC_O,       KC_P,     KC_BSLS, KC_P7,      KC_P8,     KC_P9,       RALT_T(KC_PAST),
     LSFT_T(KC_CAPS), KC_A,     KC_S,     KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,           KC_L,       KC_SCLN,  KC_QUOT, KC_P4,      KC_P5,     KC_P6,       TD(DSH_UND),
     KC_LCTL,  	     KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,        KC_DOT,     KC_SLSH,  KC_UP,   KC_P1,      KC_P2,     KC_P3,       TD(PLS_EQL),
-    KC_LALT,         KC_NO,    KC_LGUI,  TT(2),    KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  RSFT_T(KC_ENT), TT(3),      KC_LEFT,  KC_DOWN, KC_RIGHT,   KC_0,      KC_PDOT,     RCTL_T(KC_ENT)
+    KC_LALT,         KC_NO,    KC_LGUI,  TT(3),    KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  RSFT_T(KC_ENT), TT(2),      KC_LEFT,  KC_DOWN, KC_RIGHT,   KC_0,      KC_PDOT,     RCTL_T(KC_EQL)
   ),
 
 /* [2] SYMBOL
