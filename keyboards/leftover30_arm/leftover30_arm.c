c23ce9eaca3f2cdbea8ca8be7b01fc115e2bdccf
TJ Campie <tom.campie@gmail.com>
2021-08-29 19:49:12 -0500

    leftover fixes

--- keyboards/leftover30_arm/leftover30_arm.c
+++ keyboards/leftover30_arm/leftover30_arm.c
@@ -22,7 +22,7 @@ void keyboard_pre_init_user(void) {
   setPinOutput(B10);
   setPinOutput(B11);
 }
-
+#ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = { {
   // Key Matrix to LED Index
   {   0, 1, 2, 3, 4, 5, 6, 7 }
@@ -38,3 +38,4 @@ led_config_t g_led_config = { {
    */
   2, 2, 0, 0, 0, 2, 2, 0
 } };
+#endif
