0b5f262cd4171476fc375dc9b2ef1988a8a04819
TJ <tom.campie@gmail.com>
2021-09-01 21:53:10 -0500

    fixed build errors from dimple_led

--- keyboards/lazydesigners/dimple/rules.mk
+++ keyboards/lazydesigners/dimple/rules.mk
@@ -1,19 +1,11 @@
 # MCU name
 MCU = atmega32u4
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
-#
+
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes        # Audio control and System control
@@ -21,3 +13,5 @@ NKRO_ENABLE = yes            # USB Nkey Rollover
 
 # Disable unsupported hardware
 AUDIO_SUPPORTED = no
+
+DEFAULT_FOLDER = lazydesigners/dimple/staggered/rev1
