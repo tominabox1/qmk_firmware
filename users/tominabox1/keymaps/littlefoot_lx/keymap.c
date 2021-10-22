60eb2c09451117b3ee7b5e2734a9c0f149389fe2
TJ Campie <tom.campie@gmail.com>
2021-10-22 13:15:54 -0500

    adding lflx to user

---
+++ users/tominabox1/keymaps/littlefoot_lx/keymap.c
@@ -0,0 +1,42 @@
+
+#include QMK_KEYBOARD_H
+#include "quantum.h"
+
+#include "tominabox1.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+    KC_GRV,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_BSPC,
+  //,-----------------------------------------------------------------------------------------------------------.
+                  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,KC_QUOTE,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+               LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), TD(TD_DCAP), KC_H, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O),
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+                  LT(_NAV, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M, KC_COMM,  KC_DOT,     KC_SLSH,
+    KC_LALT, KC_ENT, KC_NUM_SPC,KC_NUM_SPC, KC_NAV_ENT
+  ),
+
+  [_NUM_SYM] = LAYOUT(
+    KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0, //0 + 9 = KC_BSPC
+    KC_EXLM,    KC_AT,  KC_HASH,   TD(TD_DLR_LP), KC_PERC, KC_CIRC, TD(TD_AMP_RP),  KC_ASTR,   KC_EQL,  KC_MINS,
+    KC_TRNS,  KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS, KC_RPRN,  KC_RBRC,  KC_RCBR,  KC_DOT,  KC_SLSH,
+    KC_TRNS, KC_TRNS,   KC_TRNS,KC_TRNS,  KC_TRNS
+  ),
+
+  [_NAV] = LAYOUT(
+    KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    RESET,   RGBRST,  AG_NORM,  AG_SWAP,  DEBUG,  KC_GRV ,KC_PGDN,    KC_UP,  KC_PGUP,    KC_SCLN,
+    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  SPONGEBOB,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
+    KC_TRNS,  RGB_HUD,  RGB_SAD,  RGB_VAD,  AESTHETIC,  KC_MINS,    KC_RO,  KC_COMM,   KC_DOT,  KC_BSLS,
+    TG(_GAME),  KC_TRNS,   KC_TRNS,KC_TRNS,  KC_TRNS
+  ),
+
+  [_GAME] = LAYOUT(
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_L, KC_U, KC_Y, TO(_BASE),
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_N, KC_E, KC_I, KC_O,
+  KC_LCTRL, KC_A, KC_S, KC_T, KC_F, KC_G, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO,KC_NO,KC_NO,
+    KC_LSFT, KC_SPACE, KC_TRNS,KC_NAV_ENT, KC_NO
+  ),
+};
