ef37036f95c60ad64461550f4c320d57662c792d
TJ Campie <tom.campie@gmail.com>
2021-12-02 14:12:47 -0600

    update keymaps and add jd40

--- users/tominabox1/rules.mk
+++ users/tominabox1/rules.mk
@@ -178,3 +178,17 @@ LTO_ENABLE = no
 #EXTRAFLAGS += -flto
 SRC += tominabox1.c
 endif
+
+ifeq ($(strip $(KEYBOARD)), jd40)
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+#RGBLIGHT_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = no
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+ENCODER_ENABLE = no
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = no
+#EXTRAFLAGS += -flto
+SRC += tominabox1.c
+endif
