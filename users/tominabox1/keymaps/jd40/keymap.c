25061285b25e7e01f2b29b764f547c43829fd858
TJ Campie <tom.campie@gmail.com>
2021-12-03 08:17:39 -0600

    jd40 updated

--- users/tominabox1/keymaps/jd40/keymap.c
+++ users/tominabox1/keymaps/jd40/keymap.c
@@ -1,53 +1,27 @@
 #include QMK_KEYBOARD_H
-
-#define _BL 0
-#define _AL 1
-#define _FL 2
-#define _UL 3
+#include "tominabox1.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT(
-        KC_F12,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_AL), KC_SPC,  KC_SPC,  MO(_AL), KC_LEFT, KC_DOWN, KC_RGHT
-    ),
 
-    [_AL] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_PGUP, KC_QUOT,
-        _______, _______, _______, _______, TG(_UL), TG(_UL), _______, KC_HOME, KC_PGDN, KC_END
-    ),
+	[_BASE] = LAYOUT(
+    KC_GESC, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_QUOTE, KC_BSPC,
+    KC_CTLTB,  LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), TD(TD_DCAP), KC_H, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O),
+    KC_LSFT,   LT(_NAV, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+    LT(_NAV,KC_CAPS), KC_NAV_ENT, KC_NO, KC_NUM_SPC,  KC_NUM_SPC,  KC_NAV_ENT, KC_NAV_ENT, KC_NO, KC_NO, KC_NO
+	),
 
-    [_FL] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
+	[_NUM_SYM] = LAYOUT(
+    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0, KC_BSPC,
+    KC_BSLS, KC_EXLM,    KC_AT,  KC_HASH,   TD(TD_DLR_LP), KC_PERC, KC_CIRC, TD(TD_AMP_RP),  KC_ASTR,   KC_EQL, KC_MINS,
+    KC_DEL,  KC_TRNS,  KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS, KC_RPRN,  KC_RBRC,  KC_RCBR,  KC_DOT,  KC_SLSH,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO
+	),
 
-    [_UL] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, RGB_TOG, RGB_MOD, RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-};
+	[_NAV] = LAYOUT(
+    KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  KC_PGDN,    KC_UP,  KC_PGUP, KC_NO, TD(TD_RESET),
+    KC_PIPE,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_END,
+    KC_LTOG,    KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_COMM,   KC_DOT,  KC_NO, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO
+	),
 
-void matrix_scan_user(void) {
-  // Layer LED indicators
-  // ESC led on when in function layer, WASD cluster leds enabled when on arrow cluster
-  uint32_t layer = layer_state;
-  if (layer & (1 << 1)) {
-      //gh60_wasd_leds_on();
-  } else {
-      //gh60_wasd_leds_off();
-  }
-
-  if (layer & (1 << 2)) {
-      //gh60_esc_led_on();
-  } else {
-      //gh60_esc_led_off();
-  }
 };
