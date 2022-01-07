5d498623b2e6b864409947291b86d3bee1a5a24d
TJ Campie <tom.campie@gmail.com>
2022-01-07 15:48:52 -0600

    adjust top row macros

--- users/tominabox1/keymaps/littlefoot_lx/keymap.c
+++ users/tominabox1/keymaps/littlefoot_lx/keymap.c
@@ -10,7 +10,7 @@ enum littlefoot_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_GESC,   KC_MPLY,   KC_MNXT,   KC_VOLD,   KC_VOLU,   KC_6,   KC_7,   KC_8,   KC_F8,   MUTE,
+    KC_GESC,   KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_6,   KC_VOLD,   KC_VOLU,   KC_MPLY,   MUTE,
   //,-----------------------------------------------------------------------------------------------------------.
                   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,KC_QUOTE,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM_SYM] = LAYOUT(
-    KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MPRV,   KC_MNXT,   KC_MPLY,   KC_F10,
     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0, //0 + 9 = KC_BSPC
     KC_EXLM,    KC_AT,  KC_HASH,   TD(TD_DLR_LP), KC_PERC, KC_CIRC, TD(TD_AMP_RP),  KC_ASTR,   KC_EQL,  KC_MINS,
     KC_TRNS,  KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS, KC_RPRN,  KC_RBRC,  KC_RCBR,  KC_DOT,  KC_SLSH,
