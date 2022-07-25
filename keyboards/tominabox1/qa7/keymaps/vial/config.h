d7162ec35f98d59dd8129f7b0bf5f8607e8c5885
TJ Campie <tom.campie@gmail.com>
2022-07-25 18:21:13 -0500

    updates to rules

---
+++ keyboards/tominabox1/qa7/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2022 TJ Campie (@TJ Campie)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFA, 0x8F, 0x51, 0x05, 0x53, 0x07, 0x41, 0x53}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {3,0}
