2f971bb97053bd958f552cbdaa8b418f5df6d2e0
TJ Campie <tom.campie@gmail.com>
2026-04-15 19:30:45 -0500

    updated apricot

--- keyboards/lazydesigners/apricot/config.h
+++ keyboards/lazydesigners/apricot/config.h
@@ -17,38 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
-
 
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 10
 
-#define MATRIX_ROW_PINS { B7, B2, D5, F7 }
-#define MATRIX_COL_PINS { E6, D3, C7, D4, D6, D7, B4, B5, B6, C6 }
+#define MATRIX_ROW_PINS {B7, B2, D5, F7}
+#define MATRIX_COL_PINS {E6, D3, C7, D4, D6, D7, B4, B5, B6, C6}
 
 #define DIODE_DIRECTION COL2ROW
-
-/* RBG underglow */
-#define RGB_DI_PIN B3
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-/* #define RGBLIGHT_EFFECT_CHRISTMAS       */
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-/* #define RGBLIGHT_EFFECT_KNIGHT           */
-/* #define RGBLIGHT_EFFECT_RGB_TEST         */
-/* #define RGBLIGHT_EFFECT_ALTERNATING      */
-/* #define RGBLIGHT_EFFECT_TWINKLE          */
-
-
-#define RGBLIGHT_SLEEP
-#define RGBLED_NUM 4
-/* #define RGBLIGHT_HUE_STEP 8 */
-/* #define RGBLIGHT_SAT_STEP 8 */
-/* #define RGBLIGHT_VAL_STEP 8 */
-#endif
